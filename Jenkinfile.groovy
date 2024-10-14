4e8a423b96479975d8e9f20a561d8651d88e6963
satz786 <76962771+satz786@users.noreply.github.com>
2024-10-14 18:59:19 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -1,10 +1,9 @@
- triggers {
+pipeline {
+    agent any
+
+    triggers {
         cron('H/15 * * * *')
     }
- 
-    
-node {
-
 
   
         stage('EIB-Sprin')
@@ -19,6 +18,7 @@ node {
     {
         checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/satz786/OwnProject.git']])
     }
+ 
         stage('Build_OwnProject')
         {
         
