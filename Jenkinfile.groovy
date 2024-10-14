7cf2e7bfaa0376bccf2de70d8ef3c77ce3bd9f92
satz786 <76962771+satz786@users.noreply.github.com>
2024-10-14 20:34:57 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -6,7 +6,7 @@ pipeline {
 
       parameters {
         string(name: 'BRANCH_NAME', defaultValue: 'main', description: 'Branch to build')
-        boolean(name: 'DEPLOY', defaultValue: false, description: 'Deploy after build')
+        booleanParam(name: 'DEPLOY', defaultValue: true, description: 'Deploy after build')
         choice(name: 'ENVIRONMENT', choices: ['dev', 'staging', 'prod'], description: 'Select environment')
     }
 
