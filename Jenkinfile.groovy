ee42ddee39939985bf8b8b596ac30e89c51fdccc
satz786 <76962771+satz786@users.noreply.github.com>
2024-10-14 19:03:17 +0530

    Update Jenkinfile.groovy

--- Jenkinfile.groovy
+++ Jenkinfile.groovy
@@ -9,29 +9,31 @@ pipeline {
     {
         stage('EIB-Sprin')
         {
-            
+             steps {
                 echo "EIB completed";
-                
+             } 
             
         }
 
     stage('Checkout')
     {
+         steps {
         checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/satz786/OwnProject.git']])
+         }
     }
  
         stage('Build_OwnProject')
         {
-        
+         steps {
         build 'OwnProject'
           echo "Own project";
-            
+         }
         }
         stage('Clear-EIB')
         {
-        
+         steps {
             echo "EIB cleared";
-            
+         }
         }
     }
 }
